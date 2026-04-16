0d399da0908058b3ba523caec55e954759f77c68
Tor de Vries <tor.devries@wsu.edu>
2026-04-16 17:59:38 +0000

    re-added legendary-disco demo after accidental deletion

---
+++ themes/legendary-disco/scripts.js
@@ -0,0 +1,27 @@
+/* 
+
+Legendary Disco JavaScript 
+
+Instead of document.addEventListener() with DOMContentLoaded, put your initialization code inside the "initalize()" function.
+Make sure its starting line is "export function initializePage() {" and leave in the first console log message.
+
+Your first statement should be to call confirmTheme() with the name of your theme, as shown below.
+
+*/
+
+// start your JS here
+export function initializePage() {
+
+    // call main function that outputs message to console and update page title
+    confirmTheme("Legendary Disco");
+
+    let newArticle = document.createElement("article");
+    document.body.append(newArticle);
+
+    let header = document.querySelector("header");
+    let main = document.querySelector("main");
+    let footer = document.querySelector("footer");
+
+    newArticle.append(header, main, footer);
+
+}
