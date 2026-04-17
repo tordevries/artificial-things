847e2d5abbf06bdb3841b2989636f7ea71a1d941
GM <giovanna.occhialini@wsu.edu>
2026-04-17 02:33:27 +0000

    edited code

--- themes/mitsunaga-robot/scripts.js
+++ themes/mitsunaga-robot/scripts.js
@@ -15,4 +15,14 @@ export function initializePage() {
     // call main function that outputs message to console and update page title
     confirmTheme("Mitsunaga Robot");
 
+    let newArticle = document.createElement("article");
+    document.body.append(newArticle);
+
+    let header = document.querySelector("header");
+    let main = document.querySelector("main");
+    let footer = document.querySelector("footer");
+
+    newArticle.append(header, main, footer);
+
 }
+
